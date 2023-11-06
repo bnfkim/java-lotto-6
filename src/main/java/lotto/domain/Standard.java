b75d309f9de479de20a0a9336d907fea43c926c9
bnfkim <kgr3819@gmail.com>
2023-11-06 16:21:54 +0900

    feat: 로또 당첨 기준과 금액 enum - bonusCount 추가

--- src/main/java/lotto/domain/Standard.java
+++ src/main/java/lotto/domain/Standard.java
@@ -2,17 +2,19 @@ package lotto.domain;
 
 public enum Standard {
 
-    LOTTO_1ST(6, 2000000000),
-    LOTTO_2ND(5, 3000000),
-    LOTTO_3TH(5, 1500000),
-    LOTTO_4TH(4, 50000),
-    LOTTO_5TH(3, 5000);
+    LOTTO_1ST(6,  0, 2000000000),
+    LOTTO_2ND(5, 0, 3000000),
+    LOTTO_3TH(5, 0, 1500000),
+    LOTTO_4TH(4, 1, 50000),
+    LOTTO_5TH(3, 0, 5000);
 
     final int matchingCount;
+    final int bonusCount;
     final int prizeMoney;
 
-    Standard(int matchingCount, int prizeMoney) {
+    Standard(int matchingCount, int bonusCount, int prizeMoney) {
         this.matchingCount = matchingCount;
+        this.bonusCount = bonusCount;
         this.prizeMoney = prizeMoney;
     }
 
