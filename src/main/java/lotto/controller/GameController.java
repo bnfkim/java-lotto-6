5bd982e6f0fd1dad9f2ff54aee263995b88fe914
bnfkim <kgr3819@gmail.com>
2023-11-06 17:10:56 +0900

    feat: 게임 시작 로직 추가

---
+++ src/main/java/lotto/controller/GameController.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import lotto.domain.Game;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
