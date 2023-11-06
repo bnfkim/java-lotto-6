80aeb85e009d37542ba07687049b9b739b4ab61c
bnfkim <kgr3819@gmail.com>
2023-11-06 17:10:48 +0900

    feat: 게임 시작 로직 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,9 @@
 package lotto.view;
 
 public class OutputView {
+    private final static String COUNT_MESSAGE = "개";
+    private final static String CORRECT_MESSAGE = "일치";
+    private final static String STATISTICS_MESSAGE = "당첨 통계";
+    private final static String LINE_MESSAGE = "---";
+    private final static String RATE_OF_RETURN_MESSAGE = "총 수익률은 %f입니다.";
 }
