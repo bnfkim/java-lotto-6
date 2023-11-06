57a948ba4ad1253b89722c0e1dc63e63a96f8c1b
bnfkim <kgr3819@gmail.com>
2023-11-06 13:08:24 +0900

    feat: InputView - 구입금액 입력받는 method

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.InputValidator;
+
 public class InputView {
+    private final static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private final static String INPUT_LOTTO_NUM_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String INPUT_BONUS_NUM_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public String inputMoney() {
+        System.out.println(INPUT_MONEY_MESSAGE);
+        String input = readLine();
+
+        return input;
+    }
+
+    private String readLine(){
+        return Console.readLine();
+    }
+
 }
