cc19d9cc0ab8ba78b3e4244cbe1d55674c93ae12
bnfkim <kgr3819@gmail.com>
2023-11-07 22:51:45 +0900

    feat: LottoAmount - validate check method

--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -13,4 +13,29 @@ public class LottoAmount {
     public int getAmountOfLotto(int money) {
         return money % LOTTO_PRICE;
     }
+
+    public void validateAmount(int money) {
+        validateDivisible(money);
+        validateMinAmount(money);
+        validateMaxAmount(money);
+
+    }
+
+    private void validateMinAmount(int amount) {
+        if (amount <= LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 최소 1000원 이상 입력해주세요.");
+        }
+    }
+
+    private void validateMaxAmount(int amount) {
+        if (amount <= LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 전자복권의 구입한도는 1일 10만원으로 제한되어 있습니다..");
+        }
+    }
+
+    public void validateDivisible(int money){
+        if(money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 떨어지게 입력해주세요.");
+        }
+    }
 }
