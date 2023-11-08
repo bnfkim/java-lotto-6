2e0b4ed8ebc2227e706d5c555eadf633e070ed6e
bnfkim <kgr3819@gmail.com>
2023-11-08 13:04:48 +0900

    Feat: Lotto - number validate method

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_PICK_COUNT = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateNumber(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_PICK_COUNT) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumber(List<Integer> numbers) {
+        for(int number : numbers) {
+            validateNumberRange(number);
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getLotto() {
+        return this.numbers;
+    }
 }
