79a0cdc76269b680a62ae4d93c0c07d870fe194c
bnfkim <kgr3819@gmail.com>
2023-11-08 14:52:46 +0900

    feat: Number - compareTo

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-public class Number {
+public class Number implements Comparable<Number> {
     private static final int LOTTO_NUMBER_MIN = 1;
     private static final int LOTTO_NUMBER_MAX = 45;
 
@@ -15,5 +15,10 @@ public class Number {
         if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    @Override
+    public int compareTo(Number o) {
+        return this.number - o.number;
     }
 }
